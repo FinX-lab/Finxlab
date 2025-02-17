e64f7c805432c1a98139306d124d7c094f89025e
FinX-lab <finxlabofficial@gmail.com>
2025-02-17 15:20:33 +0545

    text style change

--- src/components/Digital/Services.jsx
+++ src/components/Digital/Services.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import services from 'data/Digital/services.json';
+import React from "react";
+import { Link } from "gatsby";
+import services from "data/Digital/services.json";
 
 const Services = () => {
   return (
@@ -8,50 +8,60 @@ const Services = () => {
       <div className="container">
         <div className="row">
           <div className="col offset-lg-1">
-            <div className="section-head mb-60">
-              <h6 className="color-main text-uppercase wow fadeInUp">our services</h6>
+            <div className="section-head mb-60 text-center">
+              <h6 className="color-main text-uppercase wow fadeInUp text-center">
+                our services
+              </h6>
               <h2 className="wow fadeInUp">
-                Perfect IT Solutions <span className="fw-normal">For Your Business</span>
+                Where Technology Meets{" "}
+                <span className="fw-normal">Business Excellence </span>
               </h2>
             </div>
           </div>
         </div>
         <div className="content">
           <div className="row">
-            {
-              services.map((service, index) => (
-                <div className="col-lg-4" key={index}>
-                  <div className="service-box mb-4 wow fadeInUp" data-wow-delay={index * 0.2 + "s"}>
-                    <h5>
-                      <Link to="/page-services-5">{ service.title }</Link>
-                      <span className="num">{ service.number }</span>
-                    </h5>
-                    <div className="icon">
-                      <img src={service.icon} alt="" />
-                    </div>
-                    <div className="info">
-                      <div className="text">
-                        { service.text }
-                      </div>
-                      <div className="tags">
-                        {
-                          service.tags.map((tag, index) => (
-                            <a href="#" className="me-1" key={index}>{ tag }</a>
-                          ))
-                        }
-                      </div>
+            {services.map((service, index) => (
+              <div className="col-lg-4" key={index}>
+                <div
+                  className="service-box mb-4 wow fadeInUp"
+                  data-wow-delay={index * 0.2 + "s"}
+                >
+                  <h5>
+                    <Link to="/page-services-5">{service.title}</Link>
+                    <span className="num">{service.number}</span>
+                  </h5>
+                  <div className="icon">
+                    <img src={service.icon} alt="" />
+                  </div>
+                  <div className="info">
+                    <div className="text">{service.text}</div>
+                    <div className="tags">
+                      {service.tags.map((tag, index) => (
+                        <a href="#" className="me-1" key={index}>
+                          {tag}
+                        </a>
+                      ))}
                     </div>
                   </div>
                 </div>
-              ))
-            }
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <img src="/assets/img/services/ser_shap_l.png" alt="" className="ser_shap_l" />
-      <img src="/assets/img/services/ser_shap_r.png" alt="" className="ser_shap_r" />
+      <img
+        src="/assets/img/services/ser_shap_l.png"
+        alt=""
+        className="ser_shap_l"
+      />
+      <img
+        src="/assets/img/services/ser_shap_r.png"
+        alt=""
+        className="ser_shap_r"
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
